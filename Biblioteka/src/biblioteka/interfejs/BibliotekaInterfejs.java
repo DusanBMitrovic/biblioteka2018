3c9bf03b95e3cb43f2d767f4b4792227196b7a57
DusanBMitrovic <dule.mitrovic000@gmail.com>
2018-03-02 11:36:14 +0100

    Uveden interfejs BibliotekaInterfejs
    Premestene klase Autori i Knjiga

---
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -0,0 +1,14 @@
+package biblioteka.interfejs;
+import biblioteka.*;
+
+public interface BibliotekaInterfejs {
+
+	public void dodajKnjigu(Knjiga k);
+	
+	public void obrisiKnjigu(Knjiga k);
+	
+	
+	
+	
+	
+}
