bfb4319eec1b5adbe0b889270af08e47f79c94b4
DusanBMitrovic <dule.mitrovic000@gmail.com>
2018-03-02 11:36:42 +0100

    Uveden interfejs  BibliotekaInterfejs

--- Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -1,5 +1,6 @@
 package biblioteka.interfejs;
 import biblioteka.*;
+import java.util.*;
 
 public interface BibliotekaInterfejs {
 
@@ -7,8 +8,9 @@ public interface BibliotekaInterfejs {
 	
 	public void obrisiKnjigu(Knjiga k);
 	
+	public LinkedList<Knjiga> vratiSveKnjige();
 	
-	
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac);
 	
 	
 }
