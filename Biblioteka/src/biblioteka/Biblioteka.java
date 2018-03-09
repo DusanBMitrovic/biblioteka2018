c93871e460d73ec783a3f88e2c0e50d234232060
DusanBMitrovic <dule.mitrovic000@gmail.com>
2018-03-09 14:39:08 +0100

    Implementirana klasa Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -1,5 +1,44 @@
 package biblioteka;
+import java.util.LinkedList;
 
-public class Biblioteka {
+import biblioteka.interfejs.BibliotekaInterfejs;
 
+public class Biblioteka implements BibliotekaInterfejs {
+
+	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+	
+	
+	@Override
+	public void dodajKnjigu(Knjiga k) {
+		knjige.add(k);
+		
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga k) {
+
+		knjige.remove(k);
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		
+		LinkedList<Knjiga> novaLista = new LinkedList<Knjiga>();
+		for(int i=0;i<knjige.size();i++) {
+			if(knjige.get(i).getNaslov().contains(naslov)) novaLista.add(knjige.get(i));
+			
+		}
+		
+		return novaLista;
+	}
+
+	
+	
 }
